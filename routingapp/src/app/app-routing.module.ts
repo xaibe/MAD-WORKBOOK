5d4843ec11e225a56091065d8131f230cbcc8bf6
Alamgir Qazi <alamgirmunirqazi@gmail.com>
2019-09-30 10:20:14 +0500

    set up lecture 8

---
+++ routingapp/src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'studentslist',
+    children: [
+      {
+        path: '',
+        loadChildren:
+          './studentslist/studentslist.module#StudentslistPageModule'
+      },
+      {
+        path: ':studentid',
+        loadChildren: './student/student.module#StudentPageModule'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
